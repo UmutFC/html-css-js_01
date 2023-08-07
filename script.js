b7c175ccadd25ea4c9a28b71d95cc83d61b7de12
Furkan Umut <135552905+UmutFC@users.noreply.github.com>
2023-08-07 23:57:44 +0300

    commit
    commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Show input success message
+function showSuccess(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// Check email is valid
+function isValidMail(email) {
+  const re =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  return re.test(String(email).toLowerCase());
+}
+
+// Event Listeners
+form.addEventListener("submit", function (e) {
+  // because of form is type of submit, it will only flash the console log so we are preventing it with "e.prevenDefault()" but form will not submit it
+  e.preventDefault();
+
+  // console.log(username); //<input type="text" id="username" placeholder="Enter username">
+
+  // so we want to submit value
+  console.log(username.value);
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "E-mail is required");
+  } else if (!isValidMail(email)) {
+    showError(email, "E-mail is not valid");
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === "") {
+    showError(password2, "Password 2 is required");
+  } else {
+    showSuccess(password2);
+  }
+});
